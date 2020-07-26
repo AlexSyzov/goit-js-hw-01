9393ff7f98304243ea1e0ca443c84e892a57b6ea
Alex Syzov <sashasyzov@gmail.com>
2020-07-26 20:14:36 +0300

    Mistakes corrected

--- js/task-02.js
+++ js/task-02.js
@@ -14,18 +14,39 @@ const inventory = {
 
 const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
-  const bindedAction = action.bind(inventory, itemName);
-  bindedAction();
+  action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
 console.log(inventory.items); // ['Knife', 'Medkit']
+
+// ========================================================================
+// Можна ще так))
+// ========================================================================
+
+// const invokeInventoryAction = function (itemName, action, bindObj) {
+//   console.log(`Invoking action on ${itemName}`);
+//   const bindedAction = action.bind(inventory, itemName);
+//   bindedAction();
+// };
+
+// invokeInventoryAction("Medkit", inventory.add, inventory);
+// // Invoking action on Medkit
+// // Adding Medkit to inventory
+
+// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+// invokeInventoryAction("Gas mask", inventory.remove, inventory);
+// // Invoking action on Gas mask
+// // Removing Gas mask from inventory
+
+// console.log(inventory.items); // ['Knife', 'Medkit']
